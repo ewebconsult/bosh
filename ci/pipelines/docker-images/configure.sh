069c3c3269b460a5a968170807780555c4c6fc23
Adrian Zankich <azankich@pivotal.io>
2017-03-02 12:25:33 -0800

    WIP copy deps
    Signed-off-by: Rob Day-Reynolds <rdayreynolds@pivotal.io>

--- ci/pipelines/docker-images/configure.sh
+++ ci/pipelines/docker-images/configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 exec fly -t production set-pipeline \
-  -p bosh:docker-images \
+  -p bosh:test-docker-image \
   -c ./pipeline.yml \
   --load-vars-from <(lpass show --note "bosh:docker-images concourse secrets")
---
+++ ci/pipelines/docker-images/tasks/copy-bosh-docker-dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-docker/bosh
+
+mkdir bosh-src/ci/docker/main-bosh-docker/bosh-deployment
+cp -R bosh-deployment/* bosh-src/ci/docker/main-bosh-docker/bosh-deployment
