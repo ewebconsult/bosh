2f62b540417a7469f64ea15ee85bf741056b8cb1
Rob Day-Reynolds <rdayreynolds@pivotal.io>
2017-03-02 13:41:29 -0800

    Add new output
    Signed-off-by: Adrian Zankich <azankich@pivotal.io>

--- ci/pipelines/docker-images/tasks/copy-bosh-docker-dependencies.sh
+++ ci/pipelines/docker-images/tasks/copy-bosh-docker-dependencies.sh
@@ -4,3 +4,5 @@ cp bosh-cli/*bosh* bosh-src/ci/docker/main-bosh-docker/bosh
 
 mkdir bosh-src/ci/docker/main-bosh-docker/bosh-deployment
 cp -R bosh-deployment/* bosh-src/ci/docker/main-bosh-docker/bosh-deployment
+
+cp -R bosh-src bosh-src-with-docker-dependencies
